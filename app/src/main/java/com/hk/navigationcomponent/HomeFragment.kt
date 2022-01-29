d06fc35649197d7dcb4820e0545cefe5a59764c8
Monika_Bhasin <monika.bhasin@myparkplus.com>
2022-01-30 04:56:22 +0530

    Revert "Navigation from Home to Login and Profile Fragment acc to Conditions, Conditional Navigation"
    This reverts commit f6e3844809ca861c0d9046c209af2b90f400a0aa.

--- app/src/main/java/com/hk/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/hk/navigationcomponent/HomeFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -14,39 +13,10 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        btn_navigate_to_login.setOnClickListener {
-//            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
-//            findNavController().navigate(action)
-//        }
-
-
-        checkUserIsLogedIn().let {
-            when(it) {
-                true -> {
-                    val action = HomeFragmentDirections.actionHomeFragmentToProfileFragment()
-                    val navOptions: NavOptions = NavOptions.Builder()
-                        .build()
-
-                    findNavController().navigate(
-                        action,
-                        navOptions
-                    )
-                }
-                false -> {
-                    val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
-                    val navOptions: NavOptions = NavOptions.Builder()
-                        .build()
-
-                    findNavController().navigate(
-                        action,
-                        navOptions
-                    )
-                }
-            }
+        btn_navigate_to_login.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
         }
     }
 
-    private fun checkUserIsLogedIn(): Boolean {
-        return false
-    }
 }
