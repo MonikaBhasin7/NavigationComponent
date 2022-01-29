e625805b14e105e1543ed3813aa5a31c64b33f92
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2022-01-30 05:27:17 +0530

    navigate back to home and profile using conditional navigation

--- app/src/main/java/com/hk/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/hk/navigationcomponent/HomeFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,8 +30,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                         .build()
 
                     findNavController().navigate(
-                        action,
-                        navOptions
+                        action
                     )
                 }
                 false -> {
@@ -38,12 +39,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                         .build()
 
                     findNavController().navigate(
-                        action,
-                        navOptions
+                        action
                     )
                 }
             }
         }
+
+        findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Boolean>("LOGIN_TRIED")
+            ?.observe(viewLifecycleOwner) {
+                if(it) {
+                    Toast.makeText(requireContext(), "Sorry not able to login", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 
     private fun checkUserIsLogedIn(): Boolean {
--- app/src/main/java/com/hk/navigationcomponent/LoginFragment.kt
+++ app/src/main/java/com/hk/navigationcomponent/LoginFragment.kt
@@ -1,6 +1,45 @@
 package com.hk.navigationcomponent
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment: Fragment(R.layout.fragment_login) {
+
+    val TAG = "LoginFragment"
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        findNavController().previousBackStackEntry?.savedStateHandle?.let {
+            it.set("LOGIN_TRIED", false)
+        }
+
+        btn_submit.setOnClickListener {
+            isLoginSuccessful().let {
+                when(it) {
+                    true -> {
+                        val action = LoginFragmentDirections.actionLoginFragmentToProfileFragment()
+                        val navOptions: NavOptions = NavOptions.Builder()
+                            .setPopUpTo(R.id.loginFragment, true)
+                            .build()
+                        findNavController().navigate(action, navOptions)
+                    }
+                    false -> {
+                        findNavController().previousBackStackEntry?.savedStateHandle?.let { savedStateHandle ->
+                            savedStateHandle.set("LOGIN_TRIED", true)
+                        }
+                        findNavController().popBackStack()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun isLoginSuccessful() : Boolean{
+        return false
+    }
 }
