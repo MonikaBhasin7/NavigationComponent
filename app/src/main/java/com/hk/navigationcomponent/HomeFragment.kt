e7f776f4d9713208dac89af29e30c571d97d7b7e
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2022-01-30 04:57:53 +0530

    Home Screen to Login and Profile Screen. Conditional Navigation

--- app/src/main/java/com/hk/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/hk/navigationcomponent/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -13,10 +14,39 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btn_navigate_to_login.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
-            findNavController().navigate(action)
+//        btn_navigate_to_login.setOnClickListener {
+//            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+//            findNavController().navigate(action)
+//        }
+
+
+        checkUserIsLogedIn().let {
+            when(it) {
+                true -> {
+                    val action = HomeFragmentDirections.actionHomeFragmentToProfileFragment()
+                    val navOptions: NavOptions = NavOptions.Builder()
+                        .build()
+
+                    findNavController().navigate(
+                        action,
+                        navOptions
+                    )
+                }
+                false -> {
+                    val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+                    val navOptions: NavOptions = NavOptions.Builder()
+                        .build()
+
+                    findNavController().navigate(
+                        action,
+                        navOptions
+                    )
+                }
+            }
         }
     }
 
+    private fun checkUserIsLogedIn(): Boolean {
+        return false
+    }
 }
