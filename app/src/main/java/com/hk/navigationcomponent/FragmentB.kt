4d0697cafb81d4ec562e64653300ff558f799719
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2022-02-03 03:04:13 +0530

    Bottom Navigation, Toolbar with Navigation component

---
+++ app/src/main/java/com/hk/navigationcomponent/FragmentB.kt
@@ -0,0 +1,12 @@
+package com.hk.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class FragmentB : Fragment(R.layout.fragment_b) {
+
+}
