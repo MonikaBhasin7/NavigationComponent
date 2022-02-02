f5ee0a7a86e119b7c7a19ce212e223a829373532
Monika_Bhasin <monika.bhasin@myparkplus.com>
2022-02-03 03:02:38 +0530

    Revert "Bottom Navigation , Toolbar with Navigation component"
    This reverts commit 049323d7a12c6ff087e9202b574d72735e03563e.

--- app/src/main/java/com/hk/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/hk/navigationcomponent/MainActivity.kt
@@ -2,37 +2,10 @@ package com.hk.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.findNavController()
-
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.fragmentA, R.id.fragmentB)
-        )
-
-        setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        bottom_nav.setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
